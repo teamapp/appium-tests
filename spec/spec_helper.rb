54c8d11cf4882f246e72edc56bd8633aabfee6de
Chamitha Wijesekera <chamitha@teamapp.com>
2018-09-05 18:23:52 +1000

    Promote Appium methods to Object to allow use in classes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,28 +2,28 @@ require 'rspec'
 require 'appium_lib'
 
 def setup_driver
-    return if $driver
+  return if $driver
 
-    caps = Appium.load_appium_txt file: File.join(Dir.pwd, '../ios/appium.txt')
-    Appium::Driver.new caps, true
-  
-    $wait = Selenium::WebDriver::Wait.new(:timeout => 30)
+  caps = Appium.load_appium_txt file: File.join(Dir.pwd, '../ios/appium.txt')
+  $driver = Appium::Driver.new caps, true
+
+  $wait = Selenium::WebDriver::Wait.new(:timeout => 30)
 end
 
 def promote_methods
-    Appium.promote_singleton_appium_methods Pages
-    Appium.promote_appium_methods RSpec::Core::ExampleGroup
-  end
+  Appium.promote_appium_methods Object
+  Appium.promote_appium_methods RSpec::Core::ExampleGroup
+end
 
 setup_driver
 promote_methods
 
 RSpec.configure do |config|
-  config.before(:all) do
+  config.before(:each) do
     $driver.start_driver
     alert_dismiss
   end
-  config.after(:all) do
-    driver_quit
+  config.after(:each) do
+    $driver.driver_quit
   end
 end
